c9881089dff8f101b93ddc710fa1a2c53eff62d0
jasina007 <jjjankowski.02@gmail.com>
2023-11-11 18:58:51 +0100

    switching between pages

--- app.py
+++ app.py
@@ -7,5 +7,13 @@ app = Flask(__name__)
 def hello_world():
     return render_template('home.html')
 
+@app.route("/login")
+def log_in():
+    return render_template("login.html", code=302)
+
+@app.route("/register")
+def register():
+    return render_template("register.html", code=302)
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
