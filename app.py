3b45572e54e795cb60e622eaac17f44390897add
jasina007 <jjjankowski.02@gmail.com>
2023-11-15 14:13:10 +0100

    added authentication functionality

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, session, flash, redirect, url_for
 from flask_mysqldb import MySQL
+import hashlib
 
 app = Flask(__name__)
 
@@ -10,6 +11,7 @@ app.config['MYSQL_DB'] = 'sql11661169'
 
 mysql = MySQL(app)
 
+app.secret_key = ' '
 
 @app.route("/")
 def hello_world():
@@ -35,6 +37,35 @@ def search():
     books = cursor.fetchall()
     cursor.close()
     return render_template("search.html", books=books)
+
+
+
+@app.route("/loggedUser/reader", methods=['POST'])
+def loggedIn():
+    if request.method == 'POST' and 'email' in request.form and 'password' in request.form:
+        email = request.form['email']
+        password = request.form['password']
+        hashPassword = hashlib.md5(password.encode()).hexdigest()
+        cursor = mysql.connection.cursor()
+        cursor.execute('SELECT * FROM `czytelnicy` WHERE Email = %s AND Haslo = %s', (email, hashPassword))
+        account = cursor.fetchone()
+        
+        if account: 
+            session['loggedin'] = True
+            session['id'] = account[0]
+            session['username'] = account[4]
+            return render_template("loggedUser.html", name=account[1], surname=account[2])
+        else:
+            flash('Wprowadzono niepoprawny email lub hasło')
+            return redirect('/login')
+
+
+@app.route("/loggedOut")
+def logout():
+    session.pop('loggedin', None)
+    session.pop('id', None)
+    session.pop('username', None)
+    return render_template("index.html")
 
 
 if __name__ == '__main__':
