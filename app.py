ca1659544ae104f015add4a5d3b0315d50080682
jasina007 <jjjankowski.02@gmail.com>
2024-01-07 20:37:05 +0100

    deploy fix 1

--- app.py
+++ app.py
@@ -478,30 +478,29 @@ def genReports():
 
 
 def convertNumberToMonth(number):
-    match number:
-        case 1:
+        if number == 1:
             return "Styczeń"
-        case 2:
+        elif number == 2:
             return "Luty"
-        case 3:
+        elif number == 3:
             return "Marzec"
-        case 4:
+        elif number == 4:
             return "Kwiecień"
-        case 5:
+        elif number == 5:
             return "Maj"
-        case 6:
+        elif number == 6:
             return "Czerwiec"
-        case 7:
+        elif number == 7:
             return "Lipiec"
-        case 8:
+        elif number == 8:
             return "Sierpień"
-        case 9:
+        elif number == 9:
             return "Wrzesień"
-        case 10:
+        elif number == 10:
             return "Październik"
-        case 11:
+        elif number == 11:
             return "Listopad"
-        case 12:
+        elif number == 12:
             return "Grudzień"
 
 
